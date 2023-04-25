391b95e14f4d170d0f53b77c586407fed81b89bf
Swapnali Waghunde <swapnaliwaghunde@Swapnalis-MacBook-Pro.local>
2023-04-25 17:08:06 +0800

    added cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,13 +1,19 @@
+import {useContext} from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart-context';
 const HeaderCartButton = props => {
+    const cartCtx = useContext(CartContext);
+    const numberOfCarItems = cartCtx.items.reduce((curNumber, item ) => {
+        return curNumber + item.amount;
+    }, 0);
     return <button className={classes.button} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
         <span>Your Cart</span>
         <span className={classes.badge}>
-            3
+            {numberOfCarItems}
         </span>
     </button>
 }
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,8 +1,10 @@
+import React from 'react';
 import classes from './Input.module.css';
-const Input = props => {
-    return <div className={classes.input}>
+const Input = React.forwardRef((props, ref) => {
+    return (
+    <div className={classes.input}>
         <label htmlFor={props.input.id}>{props.label}</label>
-        <input {...props.input}/>
-    </div>
-}
+        <input ref={ref} {...props.input}/>
+    </div>);
+});
 export default Input;
