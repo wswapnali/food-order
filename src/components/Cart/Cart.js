5fad3d097b3fce55badd65a90995c4cee20c30c2
Swapnali Waghunde <swapnaliwaghunde@Swapnalis-MacBook-Pro.local>
2023-08-03 10:45:08 +0800

    HTTP requests to firebase realtime database

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,41 +1,136 @@
-import Modal from '../UI/Modal';
-import classes from './Cart.module.css';
-import CartContext from '../../store/cart-context';
-import CartItem from './CartItem'; 
-import { useContext } from 'react';
-const Cart = props => {
-    const cartCtx = useContext(CartContext);
-    const cartItemRemoveHandler = id => {
-        cartCtx.removeItem(id);
-    }        
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
+import { Fragment, useContext, useState } from "react";
+import Checkout from "./Checkout";
+const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false);
+  const [isCheckOut, setIsCheckOut] = useState();
+  const [errorMessage, setErrorMessage] = useState("");
 
-    const cartItemAddHandler = item => {
-        cartCtx.addItem({...item, amount: 1});
-    }        
-    const cartItems = <ul className={classes['cart-items']}>{
-        cartCtx.items.map(item => 
-            <CartItem key={item.id} name={item.name} amount={item.amount} price={item.price}
-                onRemove={cartItemRemoveHandler.bind(null, item.id)}
-                onAdd={cartItemAddHandler.bind(null, item)}
-            />
-            )}</ul>;
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
-            const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
 
-            const hasItems = cartCtx.items.length > 0;
-    
-    return <Modal onClose={props.onClose}>
-        {cartItems}
-        <div className={classes.total}>
-            <span>Total Amount</span>
-            <span>{totalAmount}</span>
-        </div>
-        <div className={classes.actions}>
-            <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-            {hasItems &&  <button className={classes.button}>Order</button>}
+  const orderHandler = () => {
+    setIsCheckOut(true);
+  };
 
-        </div>
+  const onCancelHandler = () => {
+    setIsCheckOut(false);
+  };
+
+  const submitOrderHandler = async (userData) => {
+    setIsSubmitting(true);
+    try {
+      const response = await fetch(
+        "https://food-app-apis-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            user: userData,
+            orderedItems: cartCtx.items,
+          }),
+        }
+      );
+
+      setIsSubmitting(false);
+      setDidSubmit(true);
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+      cartCtx.clearCart();
+    } catch (err) {
+      setErrorMessage(err.message);
+    }
+  };
+
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
+      ))}
+    </ul>
+  );
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+
+  const hasItems = cartCtx.items.length > 0;
+
+  const modalActions = (
+    <div className={classes.actions}>
+      <button className={classes["button--alt"]} onClick={props.onClose}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={classes.button} onClick={orderHandler}>
+          Order
+        </button>
+      )}
+    </div>
+  );
+
+  const cartModalContent = (
+    <Fragment>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
+      </div>
+      {isCheckOut && (
+        <Checkout
+          onCancel={onCancelHandler}
+          onSubmitOrder={submitOrderHandler}
+        />
+      )}
+
+      {!isCheckOut && modalActions}
+    </Fragment>
+  );
+
+  const isSubmittingContent = (
+    <Fragment>
+      <p>Sending order data...</p>
+    </Fragment>
+  );
+
+  const didSubmittingContent = (
+    <Fragment>
+      <p>Successfully sent the order!</p>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+      </div>
+    </Fragment>
+  );
+
+  return (
+    <Modal onClose={props.onClose}>
+      {!errorMessage && !didSubmit && !isSubmitting && cartModalContent}
+      {!errorMessage && !didSubmit && isSubmitting && isSubmittingContent}
+      {!errorMessage && didSubmit && didSubmittingContent}
+      {errorMessage && errorMessage}
     </Modal>
-} 
+  );
+};
 
 export default Cart;
---
+++ src/components/Cart/Checkout.js
@@ -0,0 +1,113 @@
+import { useRef, useState } from "react";
+import classes from "./Checkout.module.css";
+
+const isEmpty = (value) => value.trim().length === 0;
+
+const isFiveChar = (value) => value.trim().length === 5;
+
+const Checkout = (props) => {
+  const [formInputsValidity, setFormInputsValidity] = useState({
+    name: true,
+    street: true,
+    city: true,
+    postalCode: true,
+  });
+  const nameInput = useRef();
+  const streetInput = useRef();
+  const postalCodeInput = useRef();
+  const cityInput = useRef();
+  const confirmHandler = (event) => {
+    event.preventDefault();
+
+    const enteredName = nameInput.current.value;
+    const enteredStreet = streetInput.current.value;
+    const enteredPostalCode = postalCodeInput.current.value;
+    const enteredCity = cityInput.current.value;
+
+    const enteredNameIsValid = !isEmpty(enteredName);
+    const enteredStreetIsValid = !isEmpty(enteredStreet);
+    const enteredCityIsValid = !isEmpty(enteredCity);
+    const enteredPostalCodeIsValid = isFiveChar(enteredPostalCode);
+
+    setFormInputsValidity({
+      name: enteredNameIsValid,
+      street: enteredStreetIsValid,
+      city: enteredCityIsValid,
+      postalCode: enteredPostalCodeIsValid,
+    });
+
+    const formIsValid =
+      enteredNameIsValid &&
+      enteredStreetIsValid &&
+      enteredCityIsValid &&
+      enteredPostalCodeIsValid;
+
+    if (!formIsValid) {
+      return;
+    }
+
+    props.onSubmitOrder({
+      name: enteredName,
+      street: enteredStreet,
+      postalCode: enteredPostalCode,
+      city: enteredCity,
+    });
+  };
+
+  const nameControlClasses = `${classes.control} ${
+    formInputsValidity.name ? "" : classes.invalid
+  }`;
+
+  const streetControlClasses = `${classes.control} ${
+    formInputsValidity.street ? "" : classes.invalid
+  }`;
+
+  const cityControlClasses = `${classes.control} ${
+    formInputsValidity.city ? "" : classes.invalid
+  }`;
+
+  const postalCodeControlClasses = `${classes.control} ${
+    formInputsValidity.postalCode ? "" : classes.invalid
+  }`;
+
+  return (
+    <form className={classes.form} onSubmit={confirmHandler}>
+      <div className={nameControlClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" ref={nameInput} />
+        {!formInputsValidity.name && (
+          <p className={classes.error}>Please enter a valid name</p>
+        )}
+      </div>
+      <div className={streetControlClasses}>
+        <label htmlFor="street">Street</label>
+        <input type="text" id="street" ref={streetInput} />
+        {!formInputsValidity.street && (
+          <p className={classes.error}>Please enter a valid street</p>
+        )}
+      </div>
+      <div className={postalCodeControlClasses}>
+        <label htmlFor="postal">Postal Code</label>
+        <input type="text" id="postal" ref={postalCodeInput} />
+        {!formInputsValidity.postalCode && (
+          <p className={classes.error}>Please enter a valid postal code</p>
+        )}
+      </div>
+      <div className={cityControlClasses}>
+        <label htmlFor="city">City</label>
+        <input type="text" id="city" ref={cityInput} />
+        {!formInputsValidity.city && (
+          <p className={classes.error}>Please enter a valid city</p>
+        )}
+      </div>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
+    </form>
+  );
+};
+
+export default Checkout;
